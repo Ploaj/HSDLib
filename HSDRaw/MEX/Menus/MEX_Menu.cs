fca80aea99d8e53fc3a367367d7045f2b460e9e5
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2020-10-04 18:56:56 -0500

    menu struct updates

--- HSDRaw/MEX/Menus/MEX_Menu.cs
+++ HSDRaw/MEX/Menus/MEX_Menu.cs
@@ -12,11 +12,13 @@ namespace HSDRaw.MEX.Menus
 
         public HSDArrayAccessor<MEX_Menu_Definition> MenuDef { get => _s.GetReference<HSDArrayAccessor<MEX_Menu_Definition>>(0x04); set => _s.SetReference(0x04, value); }
 
-        public HSD_MatAnimJoint OptionTextures { get => _s.GetReference<HSD_MatAnimJoint>(0x08); set => _s.SetReference(0x08, value); }
+        public SBM_SISData Descriptions { get => _s.GetReference<SBM_SISData>(0x08); set => _s.SetReference(0x08, value); }
 
-        public HSD_MatAnimJoint MenuTextures { get => _s.GetReference<HSD_MatAnimJoint>(0x0C); set => _s.SetReference(0x0C, value); }
+        public HSD_MatAnimJoint OptionTextures { get => _s.GetReference<HSD_MatAnimJoint>(0x0C); set => _s.SetReference(0x0C, value); }
 
-        public SBM_SISData Descriptions { get => _s.GetReference<SBM_SISData>(0x10); set => _s.SetReference(0x10, value); }
+        public HSD_MatAnimJoint MenuTextures { get => _s.GetReference<HSD_MatAnimJoint>(0x10); set => _s.SetReference(0x10, value); }
+
+        //public HSD_MatAnimJoint PreviewTexture { get => _s.GetReference<HSD_MatAnimJoint>(0x14); set => _s.SetReference(0x14, value); }
     }
 
     public class MEX_Menu_NameFrames : HSDAccessor
@@ -36,7 +38,7 @@ namespace HSDRaw.MEX.Menus
 
         public HSDArrayAccessor<MEX_Menu_NameFrames> MenuPreviewFrames { get => _s.GetReference<HSDArrayAccessor<MEX_Menu_NameFrames>>(0x00); set => _s.SetReference(0x00, value); }
 
-        public float FirstOption { get => _s.GetFloat(0x04); set => _s.SetFloat(0x04, value); }
+        public float Unused { get => _s.GetFloat(0x04); set => _s.SetFloat(0x04, value); }
         
         public HSDArrayAccessor<MEX_Menu_OptionDef> OptionDefs { get => _s.GetReference<HSDArrayAccessor<MEX_Menu_OptionDef>>(0x08); set => _s.SetReference(0x08, value); }
 
@@ -44,9 +46,12 @@ namespace HSDRaw.MEX.Menus
 
         public byte ReturnMenu { get => _s.GetByte(0x0D); set => _s.SetByte(0x0D, value); }
 
-        public short Frame { get => _s.GetInt16(0x0E); set => _s.SetInt16(0x0E, value); }
+        public byte Frame { get => _s.GetByte(0x0E); set => _s.SetByte(0x0E, value); }
 
-        public int MenuThink { get => _s.GetInt32(0x10); set => _s.SetInt32(0x10, value); }
+        public byte HueIndex { get => _s.GetByte(0x0F); set => _s.SetByte(0x0F, value); }
+
+        [TypeConverter(typeof(HexType))]
+        public uint Init { get => (uint)_s.GetInt32(0x10); set => _s.SetInt32(0x10, (int)value); }
     }
 
     public class MEX_Menu_OptionDef : HSDAccessor
@@ -57,11 +62,10 @@ namespace HSDRaw.MEX.Menus
 
         public byte ID { get => _s.GetByte(0x01); set => _s.SetByte(0x01, value); }
 
-        public byte Frame { get => _s.GetByte(0x02); set => _s.SetByte(0x02, value); }
+        public byte DescID { get => _s.GetByte(0x02); set => _s.SetByte(0x02, value); }
 
-        public byte DescID { get => _s.GetByte(0x03); set => _s.SetByte(0x03, value); }
-        
-        [TypeConverter(typeof(HexType))]
-        public uint Init { get => (uint)_s.GetInt32(0x04); set => _s.SetInt32(0x04, (int)value); }
+        public byte OptionFrame { get => _s.GetByte(0x03); set => _s.SetByte(0x03, value); }
+
+        public byte PreviewFrame { get => _s.GetByte(0x04); set => _s.SetByte(0x04, value); }
     }
 }
